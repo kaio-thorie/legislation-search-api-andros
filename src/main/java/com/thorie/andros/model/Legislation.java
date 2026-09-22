088cbb8703e0f5169eedb4f8c297262ebcd51c91
Kaio <kaioheitor.oliveira@gmail.com>
2026-09-22 08:31:03 -0300

    feat: create legislation model

---
+++ src/main/java/com/thorie/andros/model/Legislation.java
@@ -0,0 +1,11 @@
+package com.thorie.andros.model;
+
+import java.time.LocalDate;
+
+public class Legislation {
+    LocalDate publicationDate;
+    String politicalBody;
+    String legislationTitle;
+    LocalDate legislationDate;
+    String bodyText;
+}
